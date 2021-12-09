15295913b50de009c7e6cd52729c686af82b0436
Alex Cota <cota.raul@uabc.edu.mx>
2021-12-09 12:21:46 -0800

    Agregar archivos de proyecto.

---
+++ Models/LibroVisitasBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class LibroVisitasBase
+{
+    public string leer();
+    {
+        StreamReader archivo = new StreamReader(System.Web.Hosting.HostingEnvironment.MapPath("~") + "/App_Data/Datos.txt");
+        String todo = archivo.ReadToEnd();
+        archivo.Close();
+        return todo;
+}
---
+++ Models/StreamReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class StreamReader
+{
+    public StreamReader(string v)
+    {
+        V = v;
+    }
+
+    public string V { get; }
+
+    internal string ReadToEnd()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void Close()
+    {
+        throw new NotImplementedException();
+    }
+}
